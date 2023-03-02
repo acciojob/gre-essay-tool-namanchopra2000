5459d22a28ce6a1df883a14d08e64b3f0a32115c
namanchopra2000 <117727607+namanchopra2000@users.noreply.github.com>
2023-03-03 00:47:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,10 @@
-//your code here
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
-} else {
-    ready()
-}
 
-function ready() {
-	countWords()
 // alert()
-	document.getElementById('evaluatedText').addEventListener('keyup' , countWords)
+	var r = document.getElementById('evaluatedText');
 
-}
-
- function countWords() {
-          
-            // Get the input text value
-            var text = document.getElementById('evaluatedText').value;
-          
-            // Initialize the word counter
-            var numWords = 0;
-          
-            // Loop through the text
-            // and count spaces in it 
-            for (var i = 0; i < text.length; i++) {
-                var currentCharacter = text[i];
-          
-                // Check if the character is a space
-                if (currentCharacter == " ") {
-                    numWords += 1;
-                }
-            }
-          
-            // Add 1 to make the count equal to
-            // the number of words 
-            // (count of words = count of spaces + 1)
-            numWords += 0;
-          
-            // Display it as output
+ function countWords(e) {
+             var text = e.target.value;
+	        const numWords = text.split(" ").length;
             document.getElementById('wordCount').innerHTML = numWords;
         }
+r.addEventListener('input' , countWords);
