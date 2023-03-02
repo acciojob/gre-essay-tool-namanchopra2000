2809cb4a8cde44365c48124102a04a944e7ab16f
namanchopra2000 <117727607+namanchopra2000@users.noreply.github.com>
2023-03-02 21:19:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your code here
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+wordsCount();
+
+}
+
+ function countWords() {
+          alert("hey");
+            // Get the input text value
+            var text = document
+                .getElementById("inputField").value;
+          
+            // Initialize the word counter
+            var numWords = 0;
+          
+            // Loop through the text
+            // and count spaces in it 
+            for (var i = 0; i < text.length; i++) {
+                var currentCharacter = text[i];
+          
+                // Check if the character is a space
+                if (currentCharacter == " ") {
+                    numWords += 1;
+                }
+            }
+          
+            // Add 1 to make the count equal to
+            // the number of words 
+            // (count of words = count of spaces + 1)
+            numWords += 1;
+          
+            // Display it as output
+            document.getElementById("wordCount").innerHTML = numWords;
+        }
